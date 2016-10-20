722f8fb52a3bc0921f9269d39d27880c6cf5862c
David Robideau <robideau@iastate.edu>
2016-10-20 17:59:51 -0500

    v1.5 - updated thru Oct. 2016
    *New projects:
    - Oil change
    - Microsurgery
    
    *New essays:
    - Gen. ed reflection
    - Cumulative reflection
    
    *New headshot
    
    *Updated resume/contact info

--- Scripts/interaction.js
+++ Scripts/interaction.js
@@ -55,16 +55,83 @@ var main = function() {
         $this.text("");
     });
 
+    $('#cpu').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("MIPS CPU");
+    });
+    $('#cpu').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
     $('#essay').mouseover(function() {
         var $this = $('#selector-text');
         $this.data('initialText', $this.text());
-        $this.text("IEEE Ethics Discussion");
+        $this.text("Essays");
     });
     $('#essay').mouseout(function() {
         var $this = $('#selector-text');
         $this.data('initialText', $this.text());
         $this.text("");
     });
+
+    $('#oilchange').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("Oil Change");
+    });
+    $('#oilchange').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
+    $('#microsurgery').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("VR Microsurgery Training");
+    });
+    $('#microsurgery').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
+    $('#ethics').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("IEEE Ethics Essay");
+    });
+    $('#ethics').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
+    $('#cumul').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("Cumulative Education Reflection");
+    });
+    $('#cumul').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
+    $('#gened').mouseover(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("General Education Reflection");
+    });
+    $('#gened').mouseout(function() {
+        var $this = $('#selector-text');
+        $this.data('initialText', $this.text());
+        $this.text("");
+    });
+
 
     $('#jumbotron').mouseover(function() {
         var $this = $('#selector-text');
